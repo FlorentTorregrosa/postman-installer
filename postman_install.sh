013a9a934e39e80b5518d721cf61ba7a66f8bdc3
Florent Torregrosa <florent.torregrosa@gmail.com>
2018-06-27 20:52:20 +0200

    Use sudo to remove previous Postman version.

--- postman_install.sh
+++ postman_install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Remove previous Postman version.
-rm -rf /opt/Postman
+sudo rm -rf /opt/Postman
 
 # Download and extract last Postman version.
 wget https://dl.pstmn.io/download/latest/linux64 -O postman.tar.gz
